cace764cd99735b41acaf79a0e6ad9a226d0ce3f
KennethDamCS <mrdam.kiet@gmail.com>
2024-03-26 12:45:10 -0700

    Registration form and functionality

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+
 
 # Create your views here.
 
@@ -10,7 +12,7 @@ def home(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        #Authenticate
+        # Authenticate
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
@@ -23,10 +25,27 @@ def home(request):
     else:
         return render(request, 'home.html', {})
 
+
 def logout_user(request):
     logout(request)
     messages.success(request, "You've been logged out")
     return redirect('home')
 
+
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and log in
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
