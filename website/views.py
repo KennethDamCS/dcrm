7b3e0db14786b37210089e4bf36a7b6f0cd4e02f
KennethDamCS <mrdam.kiet@gmail.com>
2024-03-27 13:00:20 -0700

    Delete customer record functionality

--- website/views.py
+++ website/views.py
@@ -62,3 +62,17 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must be logged in to view that page")
         return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Customer record deleted")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to do that")
+        return redirect('home')
+
+def add_record(request):
+    return render(request, 'add_record.html', {})
